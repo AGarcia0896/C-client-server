ed4772cde1dff62b97a8ebdb275c8e41c5544c5b
Antonio García <antonio.garcia.acosta@gmail.com>
2019-12-16 10:12:41 -0600

    C# client-server example

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.message = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(39, 74);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(236, 20);
+            this.message.TabIndex = 0;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(120, 164);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 275);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ Server/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the Socket Listener
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8080);
+            TcpClient client = default(TcpClient);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server started...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Read(); // Wait until you press any key
+            }
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                byte[] receivedBuffer = new byte[100];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                // convert bytes to string
+                //string msg = Encoding.ASCII.GetString(receivedBuffer, 0, receivedBuffer.Length);
+                StringBuilder msg = new StringBuilder();
+                foreach(byte b in receivedBuffer)
+                {
+                    if (b.Equals(00))
+                    {
+                        break;
+                    }
+                    else
+                        msg.Append(Convert.ToChar(b).ToString());
+                }
+
+                Console.WriteLine(msg.ToString() + msg.Length);
+            }
+        }
+    }
+}
